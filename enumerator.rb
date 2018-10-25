44aaaef24701e2b54b1b79aafefe6f1e72ef45c9
novandev <donovan.adams@students.makeschool.com>
2018-10-25 13:39:50 -0700

    addded the final to the porject

--- enumerator.rb
+++ enumerator.rb
@@ -1,34 +1,55 @@
 
-# This enumerator will take an array and turn its elements into an Enumerator
+# ---------- This enumerator will take an array and turn its elements into an Enumerator ----------#
 
 require "HTTParty"
+require "json"
+
 
 sampleArray = %w(First Second Third Forth Fifth)
 
 enumeratorExample = Enumerator.new do |yielder|
   # for each element in the sample array
   sampleArray.each do |elem|
-    yielder << elem
+    yielder << elem   # Place the value into a yielder for iteration
   end
 
 end
 
-puts enumeratorExample.next  # This should First
+# puts enumeratorExample.next  # This should First
 
-puts enumeratorExample.next  # This should Second
+# puts enumeratorExample.next  # This should Second
 
-puts enumeratorExample.next  # This should Third
+# puts enumeratorExample.next  # This should Third
 
-# An easier way to do this to use the method to_enum to transform an array into the Enumerable type
+
+
+
+
+# --------- An easier way to do this to use the method to_enum to transform an array into the Enumerable type ------------#
 
 array = [1, 2, 3]
 anotherEnumerator = array.to_enum
-puts "anotherEnumerator is an " + anotherEnumerator.class.to_s
 
-# Just to prove this does the exact same
+# puts "anotherEnumerator is an " + anotherEnumerator.class.to_s
 
-puts anotherEnumerator.next
+# ----------- Just to prove this does the exact same ----------#
 
-# Lets do some work on a few data structures to drive this home
+# puts anotherEnumerator.next
 
 
+
+
+
+#---------- Lets do some work on a few data structures to drive this home ----------#
+
+response =
+    HTTParty.get("https://wakatime.com/share/@novandev/20a85168-7fd4-404e-92aa-d875abfab254.json")
+
+
+responseObject =JSON.parse(response.body)     # This will turn it into a ruby hash
+
+responseEnunerator = responseObject['data'].to_enum     # Transform it into an enum
+
+# puts responseEnunerator     # Puts the entire json file into a ruby hash
+
+# puts responseEnunerator.next    # Will just print the first
