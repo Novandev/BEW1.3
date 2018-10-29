f15886c66ca81cd7c81a30896286c541b3d987ce
novandev <donovan.adams@students.makeschool.com>
2018-10-29 13:31:33 -0700

    still doing the web scraping

--- rubypract/rubyTwitter/scraper.rb
+++ rubypract/rubyTwitter/scraper.rb
@@ -6,13 +6,22 @@ require 'byebug' # This module allways you in the shell to check your variables
 
 def scraper
   url = 'https://twitter.com/DataScienceCtrl'   # This gets the url to scrape
+
   raw_page = HTTParty.get(url)    # This is just used to get the page itself Nokogiri will do the rest
-  parsed_page = Nokogiri::HTML(raw_page)  # This puts it it into hash/dictionary/object notation
-  puts parsed_page
+
+  parsed_page = Nokogiri::HTML(raw_page)  # This puts it it into Nokogiri::XML::NodeSet
+  # puts parsed_page  # Prints the parsed page as a hash
+
+
   # Now that we have the pages lets tarted the elements by CSS class( we cant by html since....well Front end code)
 
+  ds_tweets = parsed_page.css('p.tweet-text')  # Sets a variable to all of the tweets we have
+
+  # puts ds_tweets.class    # This should be the type Nokogiri::XML::NodeSet
   
-  byebug
+  puts ds_tweets.text   # This will get just the text that we need from the page it comes in as a string
+
+  # byebug  # using bye bug to test variables
 end
 
 
